8752c9aa76815cc9f9debfa66eec1ff3e8d12904
adilaroisa <adilaroisa688@gmail.com>
2024-05-21 19:15:44 +0700

    membuat agregation

--- Agregation/Agregation.cpp
+++ Agregation/Agregation.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+#include "Anak.h"
+#include "Ibu.h"
+
 
 int main()
 {
-   
+	Ibu* varIbu = new Ibu("dini");
+	Ibu* varIbu2 = new Ibu("novi");
+	Anak* varAnak1 = new Anak("tono");
+	Anak* varAnak2 = new Anak("rini");
+	Anak* varAnak3 = new Anak("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+
+	return 0;
 }
 
