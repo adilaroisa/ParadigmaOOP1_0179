af9e6c678b46e3b544d72357e283f8aec63e3e9f
adilaroisa <adilaroisa688@gmail.com>
2024-05-21 19:06:17 +0700

    membuat header jantung

---
+++ Compotition/Jantung.h
@@ -0,0 +1,14 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class Jantung {
+public:
+	Jantung() {
+		cout << "Jantung dihidupkan\n";
+	}
+	~Jantung() {
+		cout << "Jantung dimatikan\n";
+	}
+};
+#endif
+
